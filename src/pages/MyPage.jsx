e54f5e09d21c2bcd31a4c361f4ac99f62fbeda0e
TuttoShin <starprickle29@gmail.com>
2026-08-05 00:28:44 +0900

    ✨ feat: 공통 컴포넌트 - 하단 GNB바 구현

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,9 +1,9 @@
-const HomePage = () => {
+const MyPage = () => {
   return (
     <div className="p-10 text-center text-xl font-bold">
-      <h1>Home Page</h1>
+
     </div>
   );
 };
 
-export default HomePage;
+export default MyPage;
