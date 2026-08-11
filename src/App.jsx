d56ac328833b0c5454a7638f58f795cda077d081
TuttoShin <starprickle29@gmail.com>
2026-08-11 16:54:48 +0900

    🎨 design: pc 화면 여백 색 추가 및 그림자 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -15,24 +15,27 @@ function App() {
 
   return (
     <BrowserRouter>
-    {/* 
+
+      <div className="min-h-screen w-full bg-[#F1F3F5]">
+        {/* 
         모바일 웹 표준 레이아웃 컨테이너를 적용했습니다
         최대 넓이 제한, 가운데 정렬, 최소 화면 높이 꽉 채우기 등
-    */}
-      <div className="max-w-md mx-auto min-h-screen bg-white relative shadow-sm">
+        */}
+        <div className="max-w-md mx-auto min-h-screen bg-white relative">
       
-        <main className="pb-24">
-          <Routes>
-            <Route path="/dev/components" element={<CheckComponentsPage/>} />
-            <Route path="/" element={<HomePage />} />
-            <Route path="/case" element={<CasePage />} />
-            <Route path="/hospital" element={<HospitalPage />} />
-            <Route path="/my" element={<MyPage />} />
-          </Routes>
-        </main>
+          <main className="pb-24">
+            <Routes>
+              <Route path="/dev/components" element={<CheckComponentsPage/>} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="/case" element={<CasePage />} />
+              <Route path="/hospital" element={<HospitalPage />} />
+              <Route path="/my" element={<MyPage />} />
+            </Routes>
+          </main>
 
-        <Bottombar />
-
+          <Bottombar />
+      
+        </div>
       </div>
     </BrowserRouter>
   );
