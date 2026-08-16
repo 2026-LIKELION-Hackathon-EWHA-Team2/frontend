589bd16e3ff956af0945304ee5f44b28ae4564c7
TuttoShin <starprickle29@gmail.com>
2026-08-16 22:57:07 +0900

    ♻️  refactor: 병원 측 GNB 수정

--- src/components/layout/HospitalShell.jsx
+++ src/components/layout/HospitalShell.jsx
@@ -3,8 +3,8 @@ import BottomGNB from './BottomGNB';
 
 const NAV_ITEMS = [
   { path: '/hospital/home', label: '홈', icon: 'gnb-home' },
-  { path: '/hospital/consult', label: '협진', icon: 'gnb-consult' },
-  { path: '/hospital/patients', label: '환자', icon: 'gnb-patient' },
+  { path: '/hospital/consult', label: '케이스', icon: 'gnb-consult' },
+  { path: '/hospital/patients', label: '채팅', icon: 'gnb-patient' },
 ];
 
 /* 병원 측 화면 GNB shell */
